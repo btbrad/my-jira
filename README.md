654fceb748658af1903a1caf5ba0c0fc6e70ce98
btbrad <24836323+btbrad@users.noreply.github.com>
2023-01-01 17:45:39 +0800

    fix: remove dev-tool

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 - eslint、preitter、commitlint
 - json-server
 - costume hook
+
+### TypeScript
+
+- 使用 JavaScript，大部分错误都是在 runtime(运行时)的时候发现的
+- 使用 Typescript，（强类型语言），在静态代码中，就能找到其中的一些错误
+- 鸭子类型：面向接口编程，而不是面向对象编程
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { loadDevTools } from "jira-dev-tool";
 
 loadDevTools(() =>
   ReactDOM.render(
